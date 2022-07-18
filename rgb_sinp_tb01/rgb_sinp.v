3a45d3ea9c4dc707b897a7fb10f7c37102285a79
Mark Olson <mdo@ieee.org>
2022-07-17 17:33:16 -0700

    RGB Serial Input test bench 01

---
+++ rgb_sinp_tb01/rgb_sinp.v
@@ -0,0 +1,105 @@
+// Mark Olson 2022-07-16
+// (heavily) Modified to capture serial bits using WS2812b protocol
+//    see: https://cdn-shop.adafruit.com/datasheets/WS2812B.pdf
+//
+// We expect to use a 96 MHz clock; derived from the PLL based on our 12 MHz internal clock
+// For all min/max timing values, if serial is HIGH 53 to 62 clock ticks after going HIGH
+//      then bit is 1; else 0
+// Spec says if 50 microsec goes by while serial is LOW (4800 clock ticks) then that is the "stream reset"
+//      (means that next bits are for first LED in string)
+// We will treat that 50 microsec as a reset whether HIGH or LOW
+//
+// Based on: Digi-Key Introduction to FPGA #10 Metastability: Debounce signal logic without the use of a clock divider.
+//           https://www.youtube.com/watch?v=dXU1py-Od1g
+// Based on: https://forum.digikey.com/t/debounce-logic-circuit-vhdl/12573
+// Date: December 16, 2021
+// Author: Shawn Hymel
+// License: 0BSD
+
+// RGB Serial Input logic
+module  rgb_sinp #(
+    
+    // Parameters
+    parameter COUNTER_MAX = 5000,           // a little extra room in the counter (makes no difference in bit width)
+    parameter STREAM_RESET_CLKS = 4800,     // ~= 50 microsec with 96 MHz clock (PLL from 12 MHz)
+    parameter SAMPLE_TIME_CLKS   = 57       // place to sample to see if zero bit or one bit
+) (
+
+    // Inputs
+    input           clk,            // clock, expected to be 96 MHz
+    input           rst,            // reset command: reset this block
+    input           sig,            // signal value; we will do metastability mitigation and some debounce here
+    
+    // Outputs
+    output  reg     out,            // when strobe, and if (stream_reset == 0), bit value of 0 or 1
+    output  reg     strobe,         // strobes either "out" or "stream_reset"
+    output  reg     stream_reset    // when strobe, if 1 then "stream reset" (50 microsec stable value)
+);
+
+    // Calculate number of bits needed for the counter
+    localparam WIDTH = $clog2(COUNTER_MAX + 1);
+    
+    // Internal signals
+    wire            sig_edge;
+
+    // Internal storage elements
+    reg             ff_1;
+    reg             ff_2;
+    reg [WIDTH-1:0] count = 0;
+
+    // reg             debug = 0;
+    
+    // Counter starts when outputs of the two flip-flops are different
+    assign sig_edge = ff_1 ^ ff_2;
+    
+    // Logic to sample signal after a period of time
+    always @ (posedge clk or posedge rst) begin
+    
+        // Reset flip-flops
+        if (rst == 1'b1) begin
+            ff_1 <= ~sig; // do an edge when come out of reset
+            ff_2 <= 0;
+            out <= 0;
+            strobe <= 0;
+            stream_reset <= 0;
+            count <= 0;
+        
+        // If rising edge on signal, run counter and sample again
+        end else begin
+            if (sig != ff_1) begin
+                ff_1 <= sig;
+                ff_2 <= ff_1;
+                if (sig == 1'b1) begin // rising edge; restart count
+                    count <= 1;
+                    strobe <= 0;
+                    out <= 0;
+                    stream_reset <= 0;
+                end
+            end else begin // sig == ff_1
+                // debug = ~debug; // this would be to tell we are here
+                ff_1 <= sig;
+                ff_2 <= ff_1;
+                if (strobe == 1'b1) begin
+                    strobe <= 1'b0;
+                end
+                if (stream_reset == 1'b1) begin
+                    stream_reset <= 1'b0;
+                end
+                if (count < SAMPLE_TIME_CLKS) begin
+                    count <= count + 1;
+                end else if (count == SAMPLE_TIME_CLKS) begin
+                    count <= count + 1;
+                    out <= ff_2;
+                    strobe <= 1'b1;
+                end else if (count < STREAM_RESET_CLKS) begin
+                    count <= count + 1;
+                end else if (count == STREAM_RESET_CLKS) begin
+                    count <= count + 1; // only do this code once
+                    strobe <= 1;
+                    stream_reset <= 1;
+                end
+            end
+        end
+    end
+    
+endmodule
---
+++ rgb_sinp_tb01/rgb_sinp_tb.v
@@ -0,0 +1,140 @@
+// Testbench for rgb_sinp
+// Mark Olson 2022-07-17
+//
+// based on Testbench for button debounce design
+// Date: November 16, 2021
+// Author: Shawn Hymel
+// License: 0BSD
+
+// Define timescale - approx 48 MHz but we will treat as if 96 MHz
+`timescale 10 ns / 1 ps
+
+// Define our testbench
+module rgb_sinp_tb();
+
+    // Internal signals
+    wire            out;
+    
+    // Storage elements (buttons are active low!)
+    reg             clk = 0;
+    reg             rst = 0;
+    reg             inp_sig = 1;
+    // integer         i;              // Used in for loop
+    // integer         j;              // Used in for loop
+    // integer         inp_sig_prev;   // Previous input signal state
+    // integer         nbounces;       // Holds random number
+    // integer         rdelay;         // Holds random number
+    
+    
+    // Simulation time: 25000 * 1 us = 25 ms
+    localparam DURATION = 25000;
+    localparam STREAM_RESET_CLKS = 4800;
+    localparam COUNTER_MAX = STREAM_RESET_CLKS+200;
+    localparam SAMPLE_TIME_CLKS = 57;
+
+    localparam T0H_min = 48;
+    localparam T0H_max = 105;
+    localparam T1H_min = 124;
+    localparam T1H_max = 182;
+    localparam T1L_min = 57;
+    localparam T1L_max = 115;
+    localparam T0L_min = 134;
+    localparam T0L_max = 192;
+    localparam RGB_rst = 9600;
+
+
+    // Generate clock signal (about 48 MHz)
+    always begin
+        #1
+        clk = ~clk;
+    end
+    
+    // Instantiate RGB Serial Input (and debouncer) module (uses some wait time)
+    rgb_sinp #(
+        .COUNTER_MAX(COUNTER_MAX),              // a little spare room in the counter
+        .STREAM_RESET_CLKS(STREAM_RESET_CLKS),  // for "stream reset"
+        .SAMPLE_TIME_CLKS(SAMPLE_TIME_CLKS)     // sample time for 1 or 0 bit
+    ) uut (
+        .clk(clk),
+        .rst(rst),
+        .sig(inp_sig),
+        .out(out),
+        .strobe(strobe),
+        .stream_reset(stream_reset)
+    );
+    
+    // Test control: pulse reset and create some RGB bits and timeouts
+    initial begin
+    
+        // Pulse reset
+        #10
+        rst = 1;
+        inp_sig = 0;
+        #1
+        rst = 0;
+        
+        // wait some time after reset then do min-min & max-min 1 bit
+        #100
+        // min-min 1 bit
+        inp_sig = 1;
+        #T1H_min
+        inp_sig = 0;
+        #T1L_min
+        // max-min 1 bit
+        inp_sig = 1;
+        #T1H_max
+        inp_sig = 0;
+        #T1L_min
+
+        // now do min-min & max-min 0 bit
+        // min-min 0 bit
+        inp_sig = 1;
+        #T0H_min
+        inp_sig = 0;
+        #T0L_min
+        // max-min 0 bit
+        inp_sig = 1;
+        #T0H_max
+        inp_sig = 0;
+        #T0L_min
+        
+        // now do a "stream reset" with LOW
+        inp_sig = 0;
+        #RGB_rst
+
+        // min-min 1 bit
+        inp_sig = 1;
+        #T1H_min
+        inp_sig = 0;
+        #T1L_min
+        // max-min 0 bit
+        inp_sig = 1;
+        #T0H_max
+        inp_sig = 0;
+        #T0L_min
+
+        // now do a "stream reset" with HIGH
+        inp_sig = 1;
+        #RGB_rst
+        inp_sig = 1;
+        #T0L_min
+        inp_sig = 0;
+
+    end
+    
+    // Run simulation (output to .vcd file)
+    initial begin
+    
+        // Create simulation output file 
+        $dumpfile("rgb_sinp_tb.vcd");
+        $dumpvars(0, rgb_sinp_tb);
+        
+        // Wait for given amount of time for simulation to complete
+        #(DURATION)
+        
+        // Notify and end simulation
+        $display("Finished!");
+        $finish;
+    end
+    
+endmodule
