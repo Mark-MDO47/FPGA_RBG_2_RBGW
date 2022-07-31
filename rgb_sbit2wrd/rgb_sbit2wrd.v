366038e0e260129201e6e012bd0869296fe87f89
Mark Olson <mdo@ieee.org>
2022-07-31 12:21:57 -0700

    fix in_strobe vs out_strobe bug

--- rgb_sbit2wrd/rgb_sbit2wrd.v
+++ rgb_sbit2wrd/rgb_sbit2wrd.v
@@ -9,8 +9,8 @@
 // inputs:
 //      clk         expected to be 96 MHz clock
 //      rst         if high on 2-3 (or more) consecutive clock posedge: reset our circuitry
-//      strobe      high for two clocks when stream_reset and sbit_value have meaning
-//                  stream_reset    sbit_value       what it means
+//      in_strobe      high for two clocks when in_stream_reset and in_sbit_value have meaning
+//                  in_stream_reset    in_sbit_value       what it means
 //                      1               anything    50 microseconds at same level detected
 //                      0               0           bit with value "0" detected
 //                      0               1           bit with value "1" detected
@@ -18,7 +18,7 @@
 // outputs:
 //     Red/Green/Blue/Status 32-bit word and place in FIFO
 //          Status: bit7            bit6            bit5
-//                  valid           stream_reset
+//                  valid           in_stream_reset
 //
 // LIMITATIONS:
 //    none known at this time.
@@ -31,9 +31,9 @@ module  rgb_sbit2wrd /* #( // Parameters ) */
     // Inputs
     input           clk,            // clock, expected to be 96 MHz
     input           rst,            // reset command: reset this block
-    input           strobe,         // strobes either "sbit_value" or "stream_reset"
-    input           sbit_value,     // when strobe, and if (stream_reset == 0), bit value of 0 or 1
-    input           stream_reset,   // when strobe, if 1 then "stream reset" (50 microsec stable value)
+    input           in_strobe,         // strobes either "in_sbit_value" or "in_stream_reset"
+    input           in_sbit_value,     // when in_strobe, and if (in_stream_reset == 0), bit value of 0 or 1
+    input           in_stream_reset,   // when in_strobe, if 1 then "stream reset" (50 microsec stable value)
 
     
     // Outputs
@@ -48,8 +48,8 @@ module  rgb_sbit2wrd /* #( // Parameters ) */
 
     reg [1:0]       rstff = 2'b00;      // to debounce rst
     reg [4:0]       bcount = bnum_first_data_bit;      // which bit is next: 0 thru 23 (bnum_last_data_bit)
-    reg             saw_strobe = 1'b0;  // set to one when detect strobe high; zero when low again
-    reg             strobe_stretch = 1'b0; // stretch my output strobe
+    reg             saw_strobe = 1'b0;  // set to one when detect in_strobe high; zero when low again
+    reg             strobe_stretch = 1'b0; // stretch my output in_strobe
 
     // Logic
     always @ (posedge clk) begin
@@ -64,29 +64,31 @@ module  rgb_sbit2wrd /* #( // Parameters ) */
             saw_strobe  <= 1'b0;
             bcount      <= bnum_first_data_bit;
         end else begin // else non-reset processing
-            if (strobe == 1'b1) begin
+            if (out_strobe == 1'b1) begin
                 if (strobe_stretch == 1'b1) begin
                     strobe_stretch <= 1'b0;
                 end else begin
                     out_strobe <= 1'b0;
+                    out_word[bnum_valid] <= 1'b0;
+                    out_word[bnum_stream_reset] <= 1'b0;
                 end
             end
 
-            if (strobe == 1'b0) begin // end of strobe 
+            if (in_strobe == 1'b0) begin // end of in_strobe 
                 saw_strobe <= 1'b0;
-            end else if ((saw_strobe == 1'b0) && (strobe == 1'b1)) begin // rcvd data to process
+            end else if ((saw_strobe == 1'b0) && (in_strobe == 1'b1)) begin // rcvd data to process
                 saw_strobe  <= 1'b1;
-                out_word[bnum_stream_reset] <= stream_reset;
-                out_word[bcount] <= sbit_value;
-                if ((stream_reset == 1'b1) || (bcount == bnum_last_data_bit)) begin // time to strobe output
+                out_word[bnum_stream_reset] <= in_stream_reset;
+                out_word[bcount] <= in_sbit_value;
+                if ((in_stream_reset == 1'b1) || (bcount == bnum_last_data_bit)) begin // time to in_strobe output
                     strobe_stretch <= 1'b1;
                     out_strobe <= 1'b1;
                     out_word[bnum_valid] <= 1'b1;
                     bcount <= bnum_first_data_bit;
                 end else begin // we got another bit but not the last bit
                     bcount <= bcount-5'd1;
-                end // got another bit or had data to strobe out
-            end // all strobe conditions we process
+                end // got another bit or had data to in_strobe out
+            end // all in_strobe conditions we process
         end // non-reset processing
     end // always posedge clk
 endmodule
--- rgb_sbit2wrd/rgb_sbit2wrd_tb.v
+++ rgb_sbit2wrd/rgb_sbit2wrd_tb.v
@@ -11,14 +11,18 @@ module rgb_sbit2wrd_tb();
     // Internal signals
 
     // Storage elements (buttons are active low!)
-    reg             clk = 0;
-    reg             rst = 0;
-    reg             strobe = 0;         // input strobe to rgb_sbit2wrd
-    reg             sbit_value = 0;     // when strobe, and if (stream_reset == 0), bit value of 0 or 1
-    reg             stream_reset = 0;   // when strobe, if 1 then "stream reset" (50 microsec stable value)
+    reg             clk = 1'b0;
+    reg             rst = 1'b0;
+    reg             in_strobe = 1'b0;         // input in_strobe to rgb_sbit2wrd
+    reg             in_sbit_value = 1'b0;     // when in_strobe, and if (in_stream_reset == 0), bit value of 0 or 1
+    reg             in_stream_reset = 1'b0;   // when in_strobe, if 1 then "stream reset" (50 microsec stable value)
+
+    reg             bit_first  = 1'b1;
+    reg             bit_second = 1'b0;
 
     // Variables
     integer                     i;
+    integer                     j;
 
     // Simulation time: 25000 * 1 us = 25 ms
     localparam DURATION = 25000;
@@ -37,9 +41,9 @@ module rgb_sbit2wrd_tb();
         // inputs
         .clk(clk),
         .rst(rst),
-        .strobe(strobe),
-        .sbit_value(sbit_value),
-        .stream_reset(stream_reset),
+        .in_strobe(in_strobe),
+        .in_sbit_value(in_sbit_value),
+        .in_stream_reset(in_stream_reset),
         // outputs
         .out_word(out_word),
         .out_strobe(out_strobe)
@@ -57,91 +61,147 @@ module rgb_sbit2wrd_tb();
         // wait some time after reset then do various inputs
         #100
         
-        // see effects of different strobe lengths - one clock strobe
-        strobe = 1'b1;
-        sbit_value = 1'b0;
-        stream_reset = 1'b0;
+        // see effects of different in_strobe lengths - one clock in_strobe
+        in_strobe = 1'b1;
+        in_sbit_value = 1'b0;
+        in_stream_reset = 1'b0;
         #2
-        strobe = 1'b0;
-        sbit_value = 1'b0;
-        stream_reset = 1'b0;
+        in_strobe = 1'b0;
+        in_sbit_value = 1'b0;
+        in_stream_reset = 1'b0;
         #2
-        // see effects of different strobe lengths - two clocks strobe
-        strobe = 1'b1;
-        sbit_value = 1'b1;
-        stream_reset = 1'b0;
+        // see effects of different in_strobe lengths - two clocks in_strobe
+        in_strobe = 1'b1;
+        in_sbit_value = 1'b1;
+        in_stream_reset = 1'b0;
         #4
-        strobe = 1'b0;
-        sbit_value = 1'b0;
-        stream_reset = 1'b0;
+        in_strobe = 1'b0;
+        in_sbit_value = 1'b0;
+        in_stream_reset = 1'b0;
         #2
-        // see effects of different strobe lengths - three clocks strobe
-        strobe = 1'b1;
-        sbit_value = 1'b0;
-        stream_reset = 1'b0;
+        // see effects of different in_strobe lengths - three clocks in_strobe
+        in_strobe = 1'b1;
+        in_sbit_value = 1'b0;
+        in_stream_reset = 1'b0;
         #6
-        strobe = 1'b0;
-        sbit_value = 1'b0;
-        stream_reset = 1'b0;
+        in_strobe = 1'b0;
+        in_sbit_value = 1'b0;
+        in_stream_reset = 1'b0;
         #2
-        // see effects of different strobe lengths - four clocks strobe
-        strobe = 1'b1;
-        sbit_value = 1'b1;
-        stream_reset = 1'b0;
+        // see effects of different in_strobe lengths - four clocks in_strobe
+        in_strobe = 1'b1;
+        in_sbit_value = 1'b1;
+        in_stream_reset = 1'b0;
         #8
-        strobe = 1'b0;
-        sbit_value = 1'b0;
-        stream_reset = 1'b0;
+        in_strobe = 1'b0;
+        in_sbit_value = 1'b0;
+        in_stream_reset = 1'b0;
 
-        // see effects of stream_reset - two clocks strobe
+        // see effects of in_stream_reset - two clocks in_strobe
         #2
-        strobe = 1'b1;
-        sbit_value = 1'b1;
-        stream_reset = 1'b1;
+        in_strobe = 1'b1;
+        in_sbit_value = 1'b1;
+        in_stream_reset = 1'b1;
         #4
-        strobe = 1'b0;
-        sbit_value = 1'b0;
-        stream_reset = 1'b0;
+        in_strobe = 1'b0;
+        in_sbit_value = 1'b0;
+        in_stream_reset = 1'b0;
 
-        // pass some bits through the serial-to-parallel code
-        for (i = 0; i < 24; i = i + 1) begin
-            #2
-            strobe = 1'b1;
-            sbit_value = 1'b1;
-            stream_reset = 1'b0;
-            #4
-            strobe = 1'b0;
-            sbit_value = 1'b0;
-            stream_reset = 1'b0;
-            #2
-            strobe = 1'b1;
-            sbit_value = 1'b0;
-            stream_reset = 1'b0;
-            #4
-            strobe = 1'b0;
-            sbit_value = 1'b0;
-            stream_reset = 1'b0;
-        end
-        for (i = 0; i < 24; i = i + 1) begin
-            #2
-            strobe = 1'b1;
-            sbit_value = 1'b0;
-            stream_reset = 1'b0;
-            #8
-            strobe = 1'b0;
-            sbit_value = 1'b0;
-            stream_reset = 1'b0;
-            #2
-            strobe = 1'b1;
-            sbit_value = 1'b1;
-            stream_reset = 1'b0;
-            #8
-            strobe = 1'b0;
-            sbit_value = 1'b0;
-            stream_reset = 1'b0;
-        end
+        // pass some bits through the serial-to-parallel code - two clocks in_strobe
+        for (j = 0; j < 4; j = j + 1) begin
+            for (i = 0; i < 12; i = i + 1) begin
+                #2
+                in_strobe = 1'b1;
+                in_sbit_value = bit_first;
+                in_stream_reset = 1'b0;
+                #4
+                in_strobe = 1'b0;
+                in_sbit_value = 1'b0;
+                in_stream_reset = 1'b0;
+                #2
+                in_strobe = 1'b1;
+                in_sbit_value = bit_second;
+                in_stream_reset = 1'b0;
+                #4
+                in_strobe = 1'b0;
+                in_sbit_value = 1'b0;
+                in_stream_reset = 1'b0;
+                #2
+                in_strobe = 1'b1;
+                in_sbit_value = bit_first;
+                in_stream_reset = 1'b0;
+                #8
+                in_strobe = 1'b0;
+                in_sbit_value = 1'b0;
+                in_stream_reset = 1'b0;
+                #2
+                in_strobe = 1'b1;
+                in_sbit_value = bit_second;
+                in_stream_reset = 1'b0;
+                #8
+                in_strobe = 1'b0;
+                in_sbit_value = 1'b0;
+                in_stream_reset = 1'b0;
+            end // i-loop
+            bit_first = ~bit_first;
+            bit_second = ~bit_second;
+        end // j-loop
+        
+        // put stream reset with no bits in counter - two clocks in_strobe
+        #2
+        in_strobe = 1'b1;
+        in_sbit_value = bit_first;
+        in_stream_reset = 1'b1;
+        #4
+        in_strobe = 1'b0;
+        in_sbit_value = 1'b0;
+        in_stream_reset = 1'b0;
+        
+        // put stream reset with one bit in counter - two clocks in_strobe
+        #2
+        in_strobe = 1'b1;
+        in_sbit_value = bit_second;
+        in_stream_reset = 1'b0;
+        #4
+        in_strobe = 1'b0;
+        in_sbit_value = 1'b0;
+        in_stream_reset = 1'b0;
+        #2
+        in_strobe = 1'b1;
+        in_sbit_value = 1'b1;
+        in_stream_reset = 1'b1;
+        #4
+        in_strobe = 1'b0;
+        in_sbit_value = 1'b0;
+        in_stream_reset = 1'b0;
 
-    end
+        // put stream reset with 23 bits in counter - two clocks in_strobe
+        for (i = 0; i < 23; i = i + 1) begin
+            #2
+            in_strobe = 1'b1;
+            in_sbit_value = bit_first;
+            in_stream_reset = 1'b0;
+            #4
+            in_strobe = 1'b0;
+            in_sbit_value = 1'b0;
+            in_stream_reset = 1'b0;
+            bit_first = ~bit_first;
+        end // i-loop
+        #2
+        in_strobe = 1'b1;
+        in_sbit_value = bit_first;
+        in_stream_reset = 1'b1;
+        #4
+        in_strobe = 1'b0;
+        in_sbit_value = 1'b0;
+        in_stream_reset = 1'b0;
+        
+        #100
+        bit_first = ~bit_first;
+        
+
+
+    end // initial
     
     // Run simulation (output to .vcd file)
     initial begin
