a0ab586c37a0738b03b8b7249a3e8f56b893f463
Mark Olson <mdo@ieee.org>
2022-08-06 16:55:07 -0700

    more code rgb_sotp.v

--- rgb_sotp/rgb_sotp.v
+++ rgb_sotp/rgb_sotp.v
@@ -58,127 +58,211 @@ module  rgb_sotp #(
     localparam bnum_B_first_data_bit  = 5'd7;  // most significant bit highest; order G-R-B
     localparam bnum_B_last_data_bit   = 5'd0;  // most significant bit highest; order G-R-B
 
+
+    // state machine one (supervisor) gets data from FIFO and feeds serial output
+    localparam STATE1_GET_FIFO_DAT   = 4'd0;
+    localparam STATE1_CNVRT_DAT_1    = 4'd1;
+    localparam STATE1_CNVRT_DAT_2    = 4'd2;
+    localparam STATE1_CNVRT_DAT_3    = 4'd3;
+    localparam STATE1_OUT_RED        = 4'd4; // output order is R/G/B/W
+    localparam STATE1_OUT_GREEN      = 4'd5;
+    localparam STATE1_OUT_BLUE       = 4'd6;
+    localparam STATE1_OUT_LAST       = 4'd7;
+
     localparam RGBW_T0H = 16; // num of clocks to use output - see SK6812RGBW spec
     localparam RGBW_T0L = 74; // num of clocks to use output - see SK6812RGBW spec
     localparam RGBW_T1H = 45; // num of clocks to use output - see SK6812RGBW spec
     localparam RGBW_T1L = 45; // num of clocks to use output - see SK6812RGBW spec
 
-
-    // state machine one (supervisor) gets data from FIFO and feeds serial output
-    localparam state1_wait_rst       = 4'd0;
-    localparam state1_wait_start     = 4'd1;
-    localparam state1_get_fifo_dat   = 4'd2;
-    localparam state1_cnvrt_dat_1    = 4'd3;
-    localparam state1_cnvrt_dat_2    = 4'd4;
-    localparam state1_cnvrt_dat_3    = 4'd5;
-    localparam state1_out_red        = 4'd6;
-    localparam state1_out_green      = 4'd7;
-    localparam state1_out_blue       = 4'd8;
-    localparam state1_out_white      = 4'd9;
-    localparam state1_out_strm_rst   = 4'd5;
-
     // state machine 2 (serial output) outputs a byte 1 bit at a time
-    localparam state2_wait_rst       = 3'd0;
-    localparam state2_wait_start     = 3'd1;
-    localparam state2_send_bit       = 3'd2;
-    localparam state2_next_bit       = 3'd3;
-    localparam state2_send_t0h       = 3'd4;
-    localparam state2_send_t0l       = 3'd5;
-    localparam state2_send_t1h       = 3'd6;
-    localparam state2_send_t1l       = 3'd7;
+    localparam STATE2_WAIT_START    = 3'd0;
+    localparam STATE2_SEND_T0H      = 3'd1;
+    localparam STATE2_SEND_T0L      = 3'd2;
+    localparam STATE2_SEND_T1H      = 3'd3;
+    localparam STATE2_SEND_T1L      = 3'd4;
+    localparam STATE2_OUT_STRM_RST  = 3'd5;
 
     // Internal signals
     wire            sig_edge;
 
     // Internal storage elements
     reg [1:0]       rstff = 2'b00;                  // to debounce rst
-    reg [WID_MSB:0] count = WIDTH'd0;
-    reg [3:0]       state = state1_wait_rst;
+
+    // State Machine 1 storage - Executive
+    reg [3:0]       state1 = STATE1_GET_FIFO_DAT;
     // reg [7:0]       fifo_dat_status = 8'b0;
-    reg [7:0]       fifo_dat_red = 8'b0;
+    reg [7:0]       fifo_dat_red = 8'b0;        // holds red then other colors
     reg [7:0]       fifo_dat_green = 8'b0;
     reg [7:0]       fifo_dat_blue = 8'b0;
     reg [7:0]       calc_min_color = 8'b0;
+
+    // State Machine 2 storage - Serial Out
+    reg [3:0]       state2 = STATE2_WAIT_START;
+    reg [WID_MSB:0] outserial_count = WIDTH'd0;
+    reg [3:0]       outbit_count = 4'd0;
+
     // reg          debug = 0;
     
-    // Logic
-    always @ (posedge clk) begin
-        // remove metastability of rst; provide synchronous off
+
+    always @ (posedge clk) begin // Logic for state machine 1 - executive
+        // executive state machine removes metastability of rst; provide synchronous off
         if (rst == 1'b1) rstff <= 2'b11;
         else             rstff <= {rstff[0], 1'b0};
 
         if (rstff[1] == 1'b1) begin // Reset processing
-            out_sig <= 1'b0;
             out_rd_fifo_en <= 1'b0;
-            count <= WIDTH'd0;
-            state = state1_wait_fifo;
-        // If rising edge on signal, run counter and sample again
+            state1 <= state1_wait_fifo;
         end else begin
-            case (state)
+            case (state1)
                 case state1_wait_fifo: begin
                     if (1'b0 == in_rd_fifo_empty) begin
-                        out_rd_fifo_en = 1'b1;
-                        state <= state1_get_fifo_dat;
+                        out_rd_fifo_en <= 1'b1;
+                        state1 <= STATE1_GET_FIFO_DAT;
                     end
                 end // case state1_wait_fifo
-
-                case state1_get_fifo_dat: begin
-                    out_rd_fifo_en = 1'b0;
+                case STATE1_GET_FIFO_DAT: begin
+                    out_rd_fifo_en <= 1'b0;
                     if (1'b1 == in_rd_fifo_data[bnum_stream_reset]) begin
-                        count = STREAM_RESET_CLKS;
-                        state <= state1_strm_rst;
+                        outserial_count <= STREAM_RESET_CLKS;
+                        outbit_count <= 4'd15; // code for stream_reset
+                        state1 <= STATE1_OUT_LAST;
                     end else begin
                         fifo_dat_red   <= in_rd_fifo_data[bnum_R_first_data_bit:bnum_R_last_data_bit];
                         fifo_dat_green <= in_rd_fifo_data[bnum_G_first_data_bit:bnum_G_last_data_bit];
                         fifo_dat_blue  <= in_rd_fifo_data[bnum_B_first_data_bit:bnum_B_last_data_bit];
                         calc_min_color <= in_rd_fifo_data[bnum_B_first_data_bit:bnum_B_last_data_bit];
-                        state <= state1_cnvrt_dat_1;
+                        state1 <= STATE1_CNVRT_DAT_1;
                     end
                 end // case state1_wait_fifo
-
-                case state1_cnvrt_dat_1: begin
+                case STATE1_CNVRT_DAT_1: begin
                     if (calc_min_color > fifo_dat_red) calc_min_color <= fifo_dat_red;
-                    state <= state1_cnvrt_dat_2;
-                end // case state1_cnvrt_dat_1
-                case state1_cnvrt_dat_2: begin
+                    state1 <= STATE1_CNVRT_DAT_2;
+                end // case STATE1_CNVRT_DAT_1
+                case STATE1_CNVRT_DAT_2: begin
                     if (calc_min_color > fifo_dat_green) calc_min_color <= fifo_dat_green;
-                    state <= state1_cnvrt_dat_3;
-                end // case state1_cnvrt_dat_2
-                case state1_cnvrt_dat_3: begin
-                    fifo_dat_red   <= fifo_dat_red - calc_min_color;
+                    state1 <= STATE1_CNVRT_DAT_3;
+                end // case STATE1_CNVRT_DAT_2
+                case STATE1_CNVRT_DAT_3: begin
+                    fifo_dat_red   <= fifo_dat_red - calc_min_color; // use red storage for out
                     fifo_dat_green <= fifo_dat_green - calc_min_color;
                     fifo_dat_blue  <= fifo_dat_blue - calc_min_color;
+                    outbit_count <= 4'd8;
+                    state1 <= STATE1_OUT_RED;
+                end // case STATE1_CNVRT_DAT_3
+                case STATE1_OUT_RED: begin // wait for red to go then do green
+                    if (4'd0 == outbit_count) begin
+                        fifo_dat_red <= fifo_dat_green;
+                        outbit_count <= 4'd8;
+                        state1 <= STATE1_OUT_GREEN;
+                    end
+                end // case STATE1_OUT_RED
+                case STATE1_OUT_GREEN: begin // wait for green to go then do blue
+                    if (4'd0 == outbit_count) begin
+                        fifo_dat_red <= fifo_dat_blue;
+                        outbit_count <= 4'd8;
+                        state1 <= STATE1_OUT_BLUE;
+                    end
+                end // case STATE1_OUT_GREEN
+                case STATE1_OUT_BLUE: begin // wait for blue to go then do white
+                    if (4'd0 == outbit_count) begin
+                        fifo_dat_red <= calc_min_color;
+                        outbit_count <= 4'd8;
+                        state1 <= STATE1_OUT_LAST; // white is the last color
+                    end
+                end // case STATE1_OUT_BLUE
+                case STATE1_OUT_LAST: begin // wait for blue to go then do white
+                    if (4'd0 == outbit_count) begin
+                        state1 <= STATE1_GET_FIFO_DAT;
+                    end
+                end // case STATE1_OUT_LAST
+                default: state1 <= state1_wait_fifo; // Go to wait_fifo if in unknown state
+			endcase // on state1
+        end // non-reset processing for state machine 1
+    end // always for state machine 1
 
-                    state <= state1_SEND_COLORS; FIXME
-                end // case state1_cnvrt_dat_3
-
-
-                case state1_: begin
-                        state <= state1_;
-                end // case state1_
-                case state1_: begin
-                    state <= state1_;
-                end // case state1_
-                case state1_: begin
-                        state <= state1_;
-                end // case state1_
-                case state1_: begin
-                    state <= state1_;
-                end // case state1_
-                case state1_: begin
-                        state <= state1_;
-                end // case state1_
-                case state1_: begin
-                    state <= state1_;
-                end // case state1_
-                case state1_: begin
-                        state <= state1_;
-                end // case state1_
-
-                // Go to wait_fifo if in unknown state
-                default: state <= state1_wait_fifo;
-			endcase // on state
-        end
-    end // always
+    always @ (posedge clk) begin // Logic for state machine 2
+        if (rstff[1] == 1'b1) begin // Reset processing - metastable done in state machine 1
+            out_sig <= 1'b0;
+            outserial_count <= WIDTH'd0;
+            outbit_count <= 4'd0;
+            state2 <= STATE2_WAIT_START;
+        end else begin
+            case (state2)
+                case STATE2_WAIT_START: begin
+                    if (4'd15 == outbit_count) begin // stream_reset
+                        out_sig <= 1'b0;
+                        outserial_count = STREAM_RESET_CLKS;
+                    end else if (4'd0 != outbit_count) begin
+                        out_sig <= 1'b1;
+                        outbit_count = outbit_count - 5'd1;
+                        if (0'b1 == fifo_dat_red[outbit_count-1]) begin
+                            outserial_count = RGBW_T0H;
+                            state2 <= STATE2_SEND_T0H;
+                        end else begin
+                            outserial_count = RGBW_T1H;
+                            state2 <= STATE2_SEND_T1H;
+                        end
+                    end
+                end // case STATE2_WAIT_START
+                case STATE2_SEND_T0H: begin
+                    if (5'd0 != outserial_count) outserial_count = outserial_count - 1;
+                    else begin
+                        outserial_count = RGBW_T0L;
+                        state2 <= STATE2_SEND_T0L;
+                    end
+                end // case STATE2_SEND_T0H
+                case STATE2_SEND_T0L: begin
+                    if (5'd0 != outserial_count) outserial_count = outserial_count - 1;
+                    else begin
+                        if (4'd0 == outbit_count) state2 <= STATE1_GET_FIFO_DAT;
+                        else
+                            out_sig <= 1'b1;
+                            outbit_count = outbit_count - 5'd1;
+                            if (0'b1 == fifo_dat_red[outbit_count-1]) begin
+                                outserial_count = RGBW_T0H;
+                                state2 <= STATE2_SEND_T0H;
+                            end else begin
+                                outserial_count = RGBW_T1H;
+                                state2 <= STATE2_SEND_T1H;
+                            end
+                        end
+                    end // finished T0L
+                end // case STATE2_SEND_T0L
+                case STATE2_SEND_T1H: begin
+                    if (5'd0 != outserial_count) outserial_count = outserial_count - 1;
+                    else begin
+                        outserial_count = RGBW_T1L;
+                        state2 <= STATE2_SEND_T1L;
+                    end
+                end // case STATE2_SEND_T1H
+                case STATE2_SEND_T1L: begin
+                    if (5'd0 != outserial_count) outserial_count = outserial_count - 1;
+                    else begin
+                        if (4'd0 == outbit_count) state2 <= STATE1_GET_FIFO_DAT;
+                        else
+                            out_sig <= 1'b1;
+                            outbit_count = outbit_count - 5'd1;
+                            if (0'b1 == fifo_dat_red[outbit_count-1]) begin
+                                outserial_count = RGBW_T1H;
+                                state2 <= STATE2_SEND_T1H;
+                            end else begin
+                                outserial_count = RGBW_T1H;
+                                state2 <= STATE2_SEND_T1H;
+                            end
+                        end
+                    end // finished T1L
+                end // case STATE2_SEND_T1L
+                case STATE2_OUT_STRM_RST: begin // 80 microseconds of LOW
+                    out_sig <= 1'b0;
+                    if (5'd0 != outserial_count) outserial_count <= outserial_count - 5'd1;
+                    else begin
+                        outbit_count = 5'd0;
+                        state2 <= STATE1_GET_FIFO_DAT;
+                    end 
+                end // case STATE2_OUT_STRM_RST
+                default: state2 <= STATE2_WAIT_START; // Go to wait_start if in unknown state
+			endcase // on state2
+        end // non-reset processing for state machine 2
+    end // always for state machine 2
     
 endmodule
