fd6f47faadce083e5a6ae88ef9c02380bb8d3f64
Mark Olson <mdo@ieee.org>
2022-08-05 21:10:55 -0700

    add some code to rgb_sotp

--- rgb_sotp/rgb_sotp.v
+++ rgb_sotp/rgb_sotp.v
@@ -47,9 +47,10 @@ module  rgb_sotp #(
 
     // Calculate number of bits needed for the counter
     localparam WIDTH = $clog2(COUNTER_MAX + 1);
+    localparam WID_MSB = WIDTH-1;
 
-    localparam bnum_stream_reset      = 5'd30;
     localparam bnum_valid             = 5'd31;
+    localparam bnum_stream_reset      = 5'd30;
     localparam bnum_G_first_data_bit  = 5'd23; // most significant bit highest; order G-R-B
     localparam bnum_G_last_data_bit   = 5'd16; // most significant bit highest; order G-R-B
     localparam bnum_R_first_data_bit  = 5'd15; // most significant bit highest; order G-R-B
@@ -62,17 +63,37 @@ module  rgb_sotp #(
     localparam RGBW_T1H = 45; // num of clocks to use output - see SK6812RGBW spec
     localparam RGBW_T1L = 45; // num of clocks to use output - see SK6812RGBW spec
 
+    localparam state_wait_rst       = 4'd0;
+    localparam state_send_t0h       = 4'd1;
+    localparam state_send_t0l       = 4'd2;
+    localparam state_send_t1h       = 4'd3;
+    localparam state_send_t1l       = 4'd4;
+    localparam state_strm_rst       = 4'd5;
+    localparam state_wait_fifo      = 4'd6;
+    localparam state_get_fifo_dat   = 4'd7;
+    localparam state_cnvrt_dat_1    = 4'd8;
+    localparam state_cnvrt_dat_2    = 4'd9;
+    localparam state_cnvrt_dat_3    = 4'd10;
+    localparam state_ilgl_go_wait1  = 4'd11;
+    localparam state_ilgl_go_wait2  = 4'd12;
+    localparam state_ilgl_go_wait3  = 4'd13;
+    localparam state_ilgl_go_wait4  = 4'd14;
+    localparam state_ilgl_go_wait5  = 4'd15;
+
     // Internal signals
     wire            sig_edge;
 
     // Internal storage elements
     reg [1:0]       rstff = 2'b00;                  // to debounce rst
-    reg [WIDTH-1:0] count = 0;
-    // reg             debug = 0;
+    reg [WID_MSB:0] count = WIDTH'd0;
+    reg [3:0]       state = state_wait_rst;
+    // reg [7:0]       fifo_dat_status = 8'b0;
+    reg [7:0]       fifo_dat_red = 8'b0;
+    reg [7:0]       fifo_dat_green = 8'b0;
+    reg [7:0]       fifo_dat_blue = 8'b0;
+    reg [7:0]       calc_min_color = 8'b0;
+    // reg          debug = 0;
     
-    // Counter starts when outputs of the two flip-flops are different and ff_2 is HIGH
-    assign sig_edge = ff_1 ^ ff_2;
-
     // Logic
     always @ (posedge clk) begin
         // remove metastability of rst; provide synchronous off
@@ -80,9 +101,76 @@ module  rgb_sotp #(
         else             rstff <= {rstff[0], 1'b0};
 
         if (rstff[1] == 1'b1) begin // Reset processing
-            count <= 0;
+            out_sig <= 1'b0;
+            out_rd_fifo_en <= 1'b0;
+            count <= WIDTH'd0;
+            state = state_wait_fifo;
         // If rising edge on signal, run counter and sample again
         end else begin
+            case (state)
+                case state_wait_fifo: begin
+                    if (1'b0 == in_rd_fifo_empty) begin
+                        out_rd_fifo_en = 1'b1;
+                        state <= state_get_fifo_dat;
+                    end
+                end // case state_wait_fifo
+
+                case state_get_fifo_dat: begin
+                    out_rd_fifo_en = 1'b0;
+                    if (1'b1 == in_rd_fifo_data[bnum_stream_reset]) begin
+                        count = STREAM_RESET_CLKS;
+                        state <= state_strm_rst;
+                    end else begin
+                        fifo_dat_red   <= in_rd_fifo_data[bnum_R_first_data_bit:bnum_R_last_data_bit];
+                        fifo_dat_green <= in_rd_fifo_data[bnum_G_first_data_bit:bnum_G_last_data_bit];
+                        fifo_dat_blue  <= in_rd_fifo_data[bnum_B_first_data_bit:bnum_B_last_data_bit];
+                        calc_min_color <= in_rd_fifo_data[bnum_B_first_data_bit:bnum_B_last_data_bit];
+                        state <= state_cnvrt_dat_1;
+                    end
+                end // case state_wait_fifo
+
+                case state_cnvrt_dat_1: begin
+                    if (calc_min_color > fifo_dat_red) calc_min_color <= fifo_dat_red;
+                    state <= state_cnvrt_dat_2;
+                end // case state_cnvrt_dat_1
+                case state_cnvrt_dat_2: begin
+                    if (calc_min_color > fifo_dat_green) calc_min_color <= fifo_dat_green;
+                    state <= state_cnvrt_dat_3;
+                end // case state_cnvrt_dat_2
+                case state_cnvrt_dat_3: begin
+                    fifo_dat_red   <= fifo_dat_red - calc_min_color;
+                    fifo_dat_green <= fifo_dat_green - calc_min_color;
+                    fifo_dat_blue  <= fifo_dat_blue - calc_min_color;
+
+                    state <= state_SEND_COLORS; FIXME
+                end // case state_cnvrt_dat_3
+
+
+                case state_: begin
+                        state <= state_;
+                end // case state_
+                case state_: begin
+                    state <= state_;
+                end // case state_
+                case state_: begin
+                        state <= state_;
+                end // case state_
+                case state_: begin
+                    state <= state_;
+                end // case state_
+                case state_: begin
+                        state <= state_;
+                end // case state_
+                case state_: begin
+                    state <= state_;
+                end // case state_
+                case state_: begin
+                        state <= state_;
+                end // case state_
+
+                // Go to wait_fifo if in unknown state
+                default: state <= state_wait_fifo;
+			endcase // on state
         end
     end
     
