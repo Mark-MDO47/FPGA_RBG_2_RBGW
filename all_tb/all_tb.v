3fd45bba1aecf5042d18e5085ffac552447ddf5d
Mark Olson <mdo@ieee.org>
2022-08-13 12:52:12 -0700

    all modules except PLL

--- all_tb/all_tb.v
+++ all_tb/all_tb.v
@@ -1,4 +1,4 @@
-// Testbench combo2_tb
+// Testbench all_tb
 // Mark Olson 2022-08-12
 //
 
@@ -6,11 +6,11 @@
 `timescale 10 ns / 1 ps
 
 // Define our testbench
-module combo2_tb();
+module all_tb();
 
     // Settings FIFO
     localparam  DATA_SIZE = 32;
-    localparam  ADDR_SIZE = 8;
+    localparam  ADDR_SIZE = 8; // 2^8 or 256
 
     // Settings sotp - timing is faster for quicker test bench and easier checking
     /* for high-speed testing
@@ -19,7 +19,7 @@ module combo2_tb();
     localparam RGBW_T1H = 4;        // num of clocks to use output - see SK6812RGBW spec
     localparam RGBW_T1L = 4;        // num of clocks to use output - see SK6812RGBW spec
     localparam RGBW_STR_RST = 20;   // ~= 80 microsec with 96 MHz clock (PLL from 12 MHz)
-    localparam COUNTER_MAX = 7800;  // use realistic size counter
+    localparam SOTP_COUNTER_MAX = 7800;  // use realistic size counter
     */
     
     // for realistic speed testing
@@ -28,7 +28,22 @@ module combo2_tb();
     localparam RGBW_T1H = 45;        // num of clocks to use output - see SK6812RGBW spec
     localparam RGBW_T1L = 45;        // num of clocks to use output - see SK6812RGBW spec
     localparam RGBW_STR_RST = 7681;  // ~= 80 microsec with 96 MHz clock (PLL from 12 MHz)
-    localparam COUNTER_MAX = 7800;   // a little extra room in the counter (makes no difference in bit width)
+    localparam SOTP_COUNTER_MAX = 7800; // a little extra room in the counter (makes no difference in bit width)
+
+    localparam SINP_COUNTER_MAX = 5000;     // a little extra room in the counter (makes no difference in bit width)
+    localparam STREAM_RESET_CLKS = 4800;    // ~= 50 microsec with 96 MHz clock (PLL from 12 MHz)
+    localparam SAMPLE_TIME_CLKS   = 57;     // place to sample to see if zero bit or one bit
+
+    // rgb_sinp will need inputs that use this many half-clocks (divide by 2 for rising edges)
+    localparam T0H_min = 48;
+    localparam T0H_max = 105;
+    localparam T1H_min = 124;
+    localparam T1H_max = 182;
+    localparam T1L_min = 57;
+    localparam T1L_max = 115;
+    localparam T0L_min = 134;
+    localparam T0L_max = 192;
+    localparam RGB_rst = 9600;
 
 
     // Internal signals
@@ -38,24 +53,30 @@ module combo2_tb();
     wire                    w_full;
     wire                    w_en;
     wire    [DATA_SIZE-1:0] w_data;
-    wire                    rgb_wr_fifo_overflow;
+    wire                    s2wd_wr_fifo_overflow;
 
+    // Internal storage elements sinp
+    reg                     si_clk = 1'b0;
+    reg                     si_rst = 1'b0;
+    reg                     si_inp_serial = 1'b0;
+    wire                    si_2_s2wd_strobe;       // output to rgb_sbit2wrd
+    wire                    si_2_s2wd_stream_reset; // when si_2_s2wd_strobe, if 1 then "stream reset" (50 microsec stable value)
+    wire                    si_2_s2wd_value;        // when si_2_s2wd_strobe, and if (si_2_s2wd_value == 0), bit value of 0 or 1
+    
     // Internal storage elements sbit2wrd
-    reg                     rgb_sbit_strobe = 1'b0;         // input rgb_sbit_strobe to rgb_sbit2wrd
-    reg                     rgb_sbit_value = 1'b0;          // when rgb_sbit_strobe, and if (rgb_sbit_stream_reset == 0), bit value of 0 or 1
-    reg                     rgb_sbit_stream_reset = 1'b0;   // when rgb_sbit_strobe, if 1 then "stream reset" (50 microsec stable value)
-    reg                     sbit_rst = 1'b0;
-
-    // Internal storage elements sotp
-    reg                     rgb_rst = 1'b0;
-    wire                    rgbw_out_serial;
-    // reg                     r_clk = 0; use FIFO clock
+    reg                     s2wd_clk = 1'b0;
+    reg                     s2wd_rst = 1'b0;
 
     // Internal storage elements FIFO
-    reg                     r_clk = 0;
-    reg                     r_rst = 0;
-    reg                     w_clk = 0;
-    reg                     w_rst = 0;
+    reg                     r_clk = 1'b0;
+    reg                     r_rst = 1'b0;
+    reg                     w_clk = 1'b0;
+    reg                     w_rst = 1'b0;
+
+    // Internal storage elements sotp
+    reg                     so_clk = 1'b0;
+    reg                     so_rst = 1'b0;
+    wire                    so_out_serial;
 
     // testing/debugging
     reg                     bit_first  = 1'b1;
@@ -73,8 +94,40 @@ module combo2_tb();
     always begin
         #1
         r_clk <= ~r_clk;
-        w_clk <= ~w_clk;
+        w_clk <= ~r_clk;
+        s2wd_clk <= ~r_clk;
+        so_clk <= ~r_clk;
+        si_clk <= ~r_clk;
     end
+
+    // Instantiate RGB Serial Input module
+    rgb_sinp #(
+        .COUNTER_MAX(SINP_COUNTER_MAX),         // a little spare room in the counter
+        .STREAM_RESET_CLKS(STREAM_RESET_CLKS),  // for "stream reset"
+        .SAMPLE_TIME_CLKS(SAMPLE_TIME_CLKS)     // sample time for 1 or 0 bit
+    ) sinp (
+        .clk(si_clk),
+        .rst(si_rst),
+        .sig(si_inp_serial),
+        .strobe(si_2_s2wd_strobe),
+        .sbit_value(si_2_s2wd_value),
+        .stream_reset(si_2_s2wd_stream_reset)
+    );
+
+    // Instantiate RGB Serial Bits Input to LED Word module
+    rgb_sbit2wrd /* #( ) */ sbit2wrd (
+        // inputs
+        .clk(s2wd_clk),
+        .rst(s2wd_rst),
+        .in_strobe(si_2_s2wd_strobe),
+        .in_sbit_value(si_2_s2wd_value),
+        .in_stream_reset(s2wd_stream_reset),
+        .in_wr_fifo_full(w_full),
+        // outputs
+        .out_word(w_data),
+        .out_strobe(w_en),
+        .out_wr_fifo_overflow(s2wd_wr_fifo_overflow)
+    );
 
     // Instantiate FIFO
     async_fifo #(
@@ -93,94 +146,97 @@ module combo2_tb();
         .r_empty(r_empty)   //fifo in/out
     );
 
-    // Instantiate RGB Serial Bits Input to LED Word module (uses some wait time)
-    rgb_sbit2wrd /* #( ) */ uut (
-        // inputs
-        .clk(w_clk),
-        .rst(sbit_rst),
-        .in_strobe(rgb_sbit_strobe),
-        .in_sbit_value(rgb_sbit_value),
-        .in_stream_reset(rgb_sbit_stream_reset),
-        .in_wr_fifo_full(w_full),
-        // outputs
-        .out_word(w_data),
-        .out_strobe(w_en),
-        .out_wr_fifo_overflow(rgb_wr_fifo_overflow)
-    );
-
-    // Instantiate RGB Serial Bits Input to LED Word module (uses some wait time)
+    // Instantiate Serial Bits Output to RGBW LED Word module
     rgb_sotp #( 
         .RGBW_T0H(RGBW_T0H),            // num of clocks to use output - see SK6812RGBW spec
         .RGBW_T0L(RGBW_T0L),            // num of clocks to use output - see SK6812RGBW spec
         .RGBW_T1H(RGBW_T1H),            // num of clocks to use output - see SK6812RGBW spec
         .RGBW_T1L(RGBW_T1L),            // num of clocks to use output - see SK6812RGBW spec
         .RGBW_STR_RST(RGBW_STR_RST),    // ~= 80 microsec with 96 MHz clock (PLL from 12 MHz)
-        .COUNTER_MAX(COUNTER_MAX)       // a little extra room in the counter (makes no difference in bit width)
+        .COUNTER_MAX(SOTP_COUNTER_MAX)  // a little extra room in the counter (makes no difference in bit width)
     ) sotp (
         // inputs
-        .clk(r_clk),                // sotp input
-        .rst(rgb_rst),              // sotp input
+        .clk(so_clk),              // sotp input
+        .rst(so_rst),              // sotp input
         .in_rd_fifo_empty(r_empty), // sotp input
         .in_rd_fifo_data(r_data),   // sotp input
         // outputs
         .out_rd_fifo_en(r_en),      // sotp output
-        .out_sig(rgbw_out_serial)   // sotp output
+        .out_sig(so_out_serial)   // sotp output
     );
     
     // Test control: pulse reset and create some RGB bits and timeouts
     initial begin
-    
 
         // Pulse reset
         #10
         r_rst <= 1'b1;
         w_rst <= 1'b1;
-        rgb_rst <= 1'b1;
-        sbit_rst <= 1'b1;
+        si_rst <=  1'b1;
+        so_rst <= 1'b1;
+        s2wd_rst <= 1'b1;
         #50
         r_rst <= 1'b0;
         w_rst <= 1'b0;
-        rgb_rst <= 1'b0;
-        sbit_rst <= 1'b0;
+        si_rst <=  1'b0;
+        so_rst <= 1'b0;
+        s2wd_rst <= 1'b0;
         
-        // wait some time after reset then do various inputs
+        // wait some time after reset then do min-min & max-min 1 bit
         #100
-        rgb_rst <= 1'b0;
+        // min-min 1 bit
+        si_inp_serial = 1;
+        #T1H_min
+        si_inp_serial = 0;
+        #T1L_min
+        // max-min 1 bit
+        si_inp_serial = 1;
+        #T1H_max
+        si_inp_serial = 0;
+        #T1L_min
 
-        // pass some bits through the serial-to-parallel code - two clocks rgb_sbit_strobe
-        for (j = 0; j < 4; j = j + 1) begin
-            for (i = 0; i < 12; i = i + 1) begin  // 24 bits RGB data
-                #2
-                where_am_i = where_am_i + 6'd1;
-                rgb_sbit_strobe = 1'b1;
-                rgb_sbit_value = bit_first;
-                rgb_sbit_stream_reset = 1'b0;
-                #4
-                rgb_sbit_strobe = 1'b0;
-                rgb_sbit_value = 1'b0;
-                rgb_sbit_stream_reset = 1'b0;
-                #2
-                where_am_i = where_am_i + 6'd1;
-                rgb_sbit_strobe = 1'b1;
-                rgb_sbit_value = ~bit_first;
-                rgb_sbit_stream_reset = 1'b0;
-                #4
-                rgb_sbit_strobe = 1'b0;
-                rgb_sbit_value = 1'b0;
-                rgb_sbit_stream_reset = 1'b0;
-            end
-            #12
-            bit_first = ~bit_first;
-        end // j-loop
+        // now do min-min & max-min 0 bit
+        // min-min 0 bit
+        si_inp_serial = 1;
+        #T0H_min
+        si_inp_serial = 0;
+        #T0L_min
+        // max-min 0 bit
+        si_inp_serial = 1;
+        #T0H_max
+        si_inp_serial = 0;
+        #T0L_min
         
+        // now do a "stream reset" with LOW
+        si_inp_serial = 0;
+        #RGB_rst
+
+        // min-min 1 bit
+        si_inp_serial = 1;
+        #T1H_min
+        si_inp_serial = 0;
+        #T1L_min
+        // max-min 0 bit
+        si_inp_serial = 1;
+        #T0H_max
+        si_inp_serial = 0;
+        #T0L_min
+
+        // now do a "stream reset" with HIGH
+        si_inp_serial = 1;
+        #RGB_rst
+        si_inp_serial = 1;
+        #T0L_min
+        si_inp_serial = 0;
+
     end
 
         // Run simulation
     initial begin
     
         // Create simulation output file 
-        $dumpfile("combo2_tb.vcd");
-        $dumpvars(0, combo2_tb);
+        $dumpfile("all_tb.vcd");
+        $dumpvars(0, all_tb);
         
         // Wait for given amount of time for simulation to complete
         #(DURATION)
