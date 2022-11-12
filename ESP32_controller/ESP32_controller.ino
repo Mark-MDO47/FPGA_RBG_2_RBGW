8d94b0bd087f604b4d169a32c620a1a0e2f17a9a
Mark Olson <mdo@ieee.org>
2022-11-11 19:28:19 -0800

    replace FastLED.delay() with delay()

--- ESP32_controller/ESP32_controller.ino
+++ ESP32_controller/ESP32_controller.ino
@@ -61,7 +61,7 @@ void loop()
   // send the 'leds' array out to the actual LED strip
   FastLED.show();  
   // insert a delay to keep the framerate modest
-  FastLED.delay(1000/FRAMES_PER_SECOND); 
+  // FastLED.delay(1000/FRAMES_PER_SECOND); // see comments above
 
   // do some periodic updates
   EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
